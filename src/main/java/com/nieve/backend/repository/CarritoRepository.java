6204ad81ce51c017c3636096b96589baf261615f
FranciscaRuzVergara <fra.ruzv@duocuc.cl>
2025-11-20 22:57:16 -0300

    modificacion del tipo de id en el repositorio carrito

--- src/main/java/com/nieve/backend/repository/CarritoRepository.java
+++ src/main/java/com/nieve/backend/repository/CarritoRepository.java
@@ -1,7 +1,9 @@
 package com.nieve.backend.repository;
 
 import com.nieve.backend.model.Carrito;
+import com.nieve.backend.model.CarritoId;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CarritoRepository extends JpaRepository<Carrito, Long> {
+public interface CarritoRepository extends JpaRepository<Carrito, CarritoId> {
 }
